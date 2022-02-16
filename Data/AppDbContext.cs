d2299a267c18e46aa6cdd06cacdfa37d10457f66
Leonardo Freire Russi <leonardofreire.russi@icloud.com>
2022-02-16 16:45:21 -0300

    Creating Models and Migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite("DataSource=app.db;cache=shared");
+    }
+}
